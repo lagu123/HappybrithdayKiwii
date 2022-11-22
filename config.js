7f38dc959f18fbc317ca7ffc7c6c7de82677ae79
lagu123 <118196124+lagu123@users.noreply.github.com>
2022-11-23 00:33:50 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mở ikkkkk';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100052664430162';
+const eventName = 'Mừng sinh nhật em!';
+const titleCard = 'For my Queen';
+const contentCard = 'Nhân ngày đặt biệt hôm nay anh chúc Kim Quyên tuổi mới luôn khoẻ mạnh, gặp nhiều may mắn trong cuộc sống ngày càn xinh và đáng yêu hơn nữa nhá. Một lần nữa chúc em sẻ luôn có những ngày vui vẻ, hạnh phúc bên những người em yêu quý không chỉ hôm nay mà là cả sau này nữa! ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'pp.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
